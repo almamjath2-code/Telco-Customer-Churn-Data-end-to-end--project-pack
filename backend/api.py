b5a48698e9137b0eef07b3979a6494254324ec18
almamjath200-droid <almamjath200@outlook.com>
2026-05-17 09:51:14 +0530

    new chenge 3

--- backend/api.py
+++ backend/api.py
@@ -56,7 +56,7 @@ def verify_api_key(x_api_key: str):
 # --------------------------
 @app.get("/health")
 def health():
-    return {"health": " welcome home "}
+    return {"health": " welcome home to customer churn prediction api"}
 
 @app.post("/predict")
 def predict(data: CustomerData, x_api_key: str = Header(None)):
