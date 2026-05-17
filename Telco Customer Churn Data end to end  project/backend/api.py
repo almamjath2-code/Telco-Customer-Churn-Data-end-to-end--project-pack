ba7dd65896fdf8ce94a1818032c7815de10bbfac
almamjath200-droid <almamjath200@outlook.com>
2026-05-17 09:29:48 +0530

    new

--- Telco Customer Churn Data end to end  project/backend/api.py
+++ Telco Customer Churn Data end to end  project/backend/api.py
@@ -56,7 +56,7 @@ def verify_api_key(x_api_key: str):
 # --------------------------
 @app.get("/health")
 def health():
-    return {"health": " welcome "}
+    return {"health": " welcome home "}
 
 @app.post("/predict")
 def predict(data: CustomerData, x_api_key: str = Header(None)):
